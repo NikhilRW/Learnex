348aaec4bd95705417b032715bc86a7074845ee9
NikhilPersonal-Code <nikhilwankhede0707@gmail.com>
2024-09-30 17:18:22 +0530

    feat:SignUpScreen

--- src/res/strings/eng.ts
+++ src/res/strings/eng.ts
@@ -1,1 +1,2 @@
 export const gettingStartedHeaderQuote = "Grow Your Education & Level up with";
+export const SignUpSubHeader ="Let’s help you meet up your success"
--- src/screens/GettingStarted.tsx
+++ src/screens/GettingStarted.tsx
@@ -2,8 +2,11 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import MaterialsIcon from 'react-native-vector-icons/MaterialIcons';
 import { gettingStartedHeaderQuote } from '../res/strings/eng';
+import { useNavigation } from '@react-navigation/native';
+import { NavigationProps } from '../routes/Route';
 
 const GettingStarted = () => {
+  const navigate = useNavigation<NavigationProps>();  
   return (
     <View className="w-screen h-screen bg-white flex justify-center items-center px-[15%] py-[20%] relative ">
       <MaterialsIcon name='circle'  color={"#37B6F0"} size={225} style={StyleSheets.cricle1}  />
@@ -20,7 +23,9 @@ const GettingStarted = () => {
             <Text className='text-[#37B6F0] text-[9vw] font-bold'>Learnex</Text>
           </View>
         </View>
-      <TouchableOpacity activeOpacity={0.65} className='bg-[#3EB9F1] px-[15%] py-[8%] rounded-3xl w-full'>
+      <TouchableOpacity activeOpacity={0.65}
+      onPress={()=>navigate.navigate("SignUp")}
+      className='bg-[#3EB9F1] px-[15%] py-[8%] rounded-3xl w-full'>
         <Text className='text-white text-center text-2xl font-bold'>Getting Started</Text>
       </TouchableOpacity>
       </View>
